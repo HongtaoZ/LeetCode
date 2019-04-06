7403c9186a75616d7d0c315f0eaab9f93e3ffe70
neil <neil@neil.com>
2019-04-06 07:56:46 -0700

    maintains two lists solution

--- python/155. Min Stack.py
+++ python/155. Min Stack.py
@@ -2,34 +2,46 @@ class MinStack:
 
     def __init__(self):
         """
-        initialize your data structure here.
+        maintain two lists, one of them contain minimum values
         """
-        self.data = list()
+        self.data = []
+        self.minData = []
+        
 
     def push(self, x: int) -> None:
         self.data.append(x)
+        # if new vaule is less than or equal to current minimum value
+        # keep storing minimum values, e.g. 50(push) 40(push) 42 43 45 30(push), 
+        # so when pop value, the minimum stack still have smallest value 
+        if len(self.minData)==0 or x<=self.minData[-1]:
+            self.minData.append(x)
 
     def pop(self) -> None:
-        self.data.pop()
+        if(len(self.data)==0):
+            return None
+        else:
+            if self.data[-1] == self.minData[-1]:
+                self.minData.pop()
+            self.data.pop()
 
     def top(self) -> int:
         if len(self.data)==0:
             return None
         else:
-            return self.data[self.data.__len__()-1]
+            return self.data[-1]
 
     def getMin(self) -> int:
         if len(self.data)==0:
             return None
         else:
-            return min(self.data)
-
+            return self.minData[-1]
 
 # Your MinStack object will be instantiated and called as such:
 obj = MinStack()
-x=50
-obj.push(x)
-obj.push(x)
+obj.push(60)
+obj.push(50)
+obj.push(52)
+obj.push(40)
 obj.pop()
 param_3 = obj.top()
 param_4 = obj.getMin()
